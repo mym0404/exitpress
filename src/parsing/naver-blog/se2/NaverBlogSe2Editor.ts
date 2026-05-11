6214cbb782390ea5909cd27c628fafeefe71b0da
MJ Studio <mym0404@gmail.com>
2026-05-11 11:30:20 +0900

    [📦 New Block] SE2 inline style video parser (#8)

--- src/parsing/naver-blog/se2/NaverBlogSe2Editor.ts
+++ src/parsing/naver-blog/se2/NaverBlogSe2Editor.ts
@@ -14,6 +14,7 @@ import { NaverSe2InlineGifVideoBlock } from "./blocks/InlineGifVideoBlock.js"
 import { NaverSe2LineBreakBlock } from "./blocks/LineBreakBlock.js"
 import { NaverSe2QuoteBlock } from "./blocks/QuoteBlock.js"
 import { NaverSe2SpacerBlock } from "./blocks/SpacerBlock.js"
+import { NaverSe2StyleBlock } from "./blocks/StyleBlock.js"
 import { NaverSe2TableBlock } from "./blocks/TableBlock.js"
 import { NaverSe2TextElementBlock } from "./blocks/TextElementBlock.js"
 import { NaverSe2TextNodeBlock } from "./blocks/TextNodeBlock.js"
@@ -23,6 +24,7 @@ export class NaverBlogSE2Editor extends BaseEditor {
   override readonly label = "SmartEditor 2"
 
   protected override readonly supportedBlocks = [
+    new NaverSe2StyleBlock(),
     new NaverSe2CommentBlock(),
     new NaverSe2TextNodeBlock(),
     new NaverSe2BookWidgetBlock(),
